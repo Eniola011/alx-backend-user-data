3a7138303e1f8e1c67bdfeef18650c9198dc0296
Eniola011 <yettyagbalu@gmail.com>
2024-04-12 11:31:22 -0700

    obscure specified fields from log records.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,15 +26,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
+    """
+        >> Redacting Formatter class: accepts a list..
+           of strings fields constructor argument.
+        >> Redacts specified fields from log records.
+    """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        """
+            >> Filters values in incoming log records using filter_datum.
+            >> Values for fields in fields should be filtered.
+            >> Do not extrapolate FORMAT manually.
+        """
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
