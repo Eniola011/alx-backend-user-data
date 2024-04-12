d096328b7984e62c8f3793f7fbbb8a6137f9c5e2
Eniola011 <yettyagbalu@gmail.com>
2024-04-12 11:21:03 -0700

    adjusted main.py files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ Filtered Datum
 
 
 from typing import List
+import logging
 import re
 
 
@@ -22,3 +23,18 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """
     regex = '|'.join(f'(?<={field}=)[^{separator}]+' for field in fields)
     return re.sub(regex, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
