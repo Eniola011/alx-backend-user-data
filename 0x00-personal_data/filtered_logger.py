102e5173f6c7a1d7a901ad8290bfc1b829f28f35
Eniola011 <yettyagbalu@gmail.com>
2024-04-13 01:02:23 +0000

    updated filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -80,10 +80,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
            > MySQLConnection: A connection object to the MySQL database.
     """
     db_connection = mysql.connector.connection.MySQLConnection(
-        user=getenv("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-        host=getenv("PERSONAL_DATA_DB_HOST", "localhost"),
-        database=getenv("PERSONAL_DATA_DB_NAME")
+        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME')
     )
 
     return db_connection
