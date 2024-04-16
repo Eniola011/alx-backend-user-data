369bc99cb94d68a04e9d0b556be2d2f558d0f60c
Eniola011 <yettyagbalu@gmail.com>
2024-04-16 08:34:16 +0000

    base64 encoding on the authorization_header

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -13,4 +13,17 @@ class BasicAuth(Auth):
     """
         >> Inherits from Auth class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        """
+            >> Returns the Base64 part of the Authorization header..
+               for a Basic Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        auth_value = authorization_header.split(' ')
+        return auth_value[1]
