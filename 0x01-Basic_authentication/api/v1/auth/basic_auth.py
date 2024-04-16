1709f0a9127dfafef15f20dd4e4ee612377c4c36
Eniola011 <yettyagbalu@gmail.com>
2024-04-16 15:59:39 +0000

    Task 12. Basic - Allow password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,6 +65,8 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         credentials = decoded_base64_authorization_header.split(':', 1)
+        if len(credentials) != 2:
+            return None, None
         return credentials[0], credentials[1]
 
     def user_object_from_credentials(self,
