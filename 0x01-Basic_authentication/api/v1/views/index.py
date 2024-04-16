9583fb136315b1ccd1930c571db9003fd7490b5d
Eniola011 <yettyagbalu@gmail.com>
2024-04-16 09:05:44 +0000

    updated:api/v1/views/index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
         >> GET /api/v1/unauthorized
@@ -45,7 +45,7 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
         >> GET /api/v1/forbidden
