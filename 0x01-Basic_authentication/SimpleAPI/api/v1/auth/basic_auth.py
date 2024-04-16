519555df9de04fa4d22dce81f9185f64824dfa7f
Eniola011 <yettyagbalu@gmail.com>
2024-04-16 08:57:33 +0000

    Task 8. Basic - Base64 decode

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ Basic Auth
 """
 
 
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -13,8 +14,9 @@ class BasicAuth(Auth):
     """
         >> Inherits from Auth class
     """
-    def extract_base64_authorization_header(self, authorization_header: str) \
-            -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header:
+                                            str) -> str:
         """
             >> Returns the Base64 part of the Authorization header..
                for a Basic Authentication.
@@ -27,3 +29,22 @@ class BasicAuth(Auth):
             return None
         auth_value = authorization_header.split(' ')
         return auth_value[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+            >> Returns the decoded value of a Base64 string..
+               base64_authorization_header.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decode_base64 = b64decode(base64_bytes)
+            value = decode_base64.decode('utf-8')
+            return value
+        except Exception:
+            return None
