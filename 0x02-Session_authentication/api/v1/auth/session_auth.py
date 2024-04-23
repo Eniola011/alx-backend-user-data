c9913c7e198fade803615517f74aa0dd0568b5d0
Eniola011 <yettyagbalu@gmail.com>
2024-04-23 03:56:19 +0000

    Task2: create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,7 @@ Session Authentication
 
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -15,4 +16,14 @@ class SessionAuth(Auth):
        >> Validates if everything inherits correctly without any overloading.
        >> Validates the “switch” by using environment variables.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+           >> Creates a Session ID for a user_id.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
