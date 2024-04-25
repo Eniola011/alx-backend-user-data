05f3e8b3d5786402081df6ccd5e353dc33284df3
Eniola011 <yettyagbalu@gmail.com>
2024-04-25 13:41:58 +0000

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,7 +65,7 @@ def logout() -> str:
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user_id)
-        return jsonify({"message": "logout successful"}), 200
+        return redirect('/')
     else:
         abort(403)
 
